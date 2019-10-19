43258dde20b9fa1da2784e2b5552e428bc037d21
Daniel Rataj <daniel.rataj@centrum.cz>
2019-10-19 16:34:21 +0200

    Update deploy process

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,9 +47,9 @@ module.exports = {
     'prefer-promise-reject-errors': 'off',
 
     // allow console.log during development only
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     quotes: [2, 'single', { avoidEscape: true }]
   }
-};
+}
